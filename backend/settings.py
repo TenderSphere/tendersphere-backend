08ad2a6e613f5c5bda6fd3e3ec23cdc954db05f2
Gopi <gopix147@gmail.com>
2026-07-31 09:49:04 +0530

    Fix static files styling for admin panel

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "https://tendersphere.github.io",
 ]  # ok for testing, we'll restrict this later
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
