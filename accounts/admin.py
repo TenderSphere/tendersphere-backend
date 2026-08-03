ef631c8074987e0762b80ad98fd3411fe79e7947
Gopi <gopix147@gmail.com>
2026-08-04 00:47:46 +0530

    Register Tender model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,3 +4,16 @@ from .models import UserProfile
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone', 'state', 'tender_interests')
+
+from django.contrib import admin
+from .models import UserProfile, Tender
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'state', 'tender_interests')
+
+@admin.register(Tender)
+class TenderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'state', 'deadline', 'reference_number')
+    list_filter = ('category', 'state')
+    search_fields = ('title', 'department', 'reference_number')
